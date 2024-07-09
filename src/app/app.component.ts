56bdff1ad9e8fd0de42ac968bb46c7c23ce16dd3
Tom Kedem <tompha1@hotmail.com>
2024-07-09 07:43:24 +0300

    Working with Outputs & Emitting Data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,5 +11,8 @@ import { DUMMY_USERS } from './dummy-users';
     imports: [HeaderComponent, UserComponent]
 })
 export class AppComponent {
-    users = DUMMY_USERS;    
+    users = DUMMY_USERS;   
+    onSelectUser(id: string) {
+        console.log('User selected: ', id);
+    }
 }
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, computed, Input, input } from '@angular/core';
+import { Component, computed, EventEmitter, Input, input , Output} from '@angular/core';
 
 
 
@@ -10,17 +10,21 @@ import { Component, computed, Input, input } from '@angular/core';
   styleUrl: './user.component.css'
 })
 export class UserComponent {
+  @Input({required: true}) id!: string;
   
-  //@Input({required: true}) avatar!: string;
-  // @Input({required: true}) name!: string;
-  avatar = input.required<string>();
-  name = input.required<string>();
+  @Input({required: true}) avatar!: string;
+  @Input({required: true}) name!: string;
+  @Output() select = new EventEmitter();
+  // avatar = input.required<string>();
+  // name = input.required<string>();
 
-  imagePhat = computed(() => `assets/users/${this.avatar()}`);
-  
-  // get imagePhat() {
-  //   return `assets/users/${this.avatar()}`;
-  // }
+  // imagePhat = computed(() => `assets/users/${this.avatar()}`);
 
-  onSelectedUser() {}
+  get imagePhat() {
+    return `assets/users/${this.avatar}`;
+  }
+
+  onSelectedUser() {
+    this.select.emit(this.id);
+  }
 }
