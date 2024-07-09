5b32d0c5d8a5ef68e5345a239a8abaf0822ebbcd
Tom Kedem <tompha1@hotmail.com>
2024-07-09 09:38:37 +0300

    Exercise: Create a Configurable Component

---
+++ src/app/app-task/app-task.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-app-task',
+  standalone: true,
+  imports: [],
+  templateUrl: './app-task.component.html',
+  styleUrl: './app-task.component.css'
+})
+export class AppTaskComponent {
+  @Input({required: true}) name!: string;
+
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,17 +2,25 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from "./user/user.component";
 import { DUMMY_USERS } from './dummy-users';
+import { AppTaskComponent } from './app-task/app-task.component';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [HeaderComponent, UserComponent]
+    imports: [HeaderComponent, UserComponent,AppTaskComponent]
 })
 export class AppComponent {
     users = DUMMY_USERS;   
+    selectedUserId='u1';
+
+    get selectedUser(){
+        return this.users.find(user => user.id === this.selectedUserId)!;
+    }
     onSelectUser(id: string) {
-        console.log('User selected: ', id);
+        this.selectedUserId= id;
+        console.log(this.selectedUserId);
+        
     }
 }
