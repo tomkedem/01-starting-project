9f1790e59d6a9bbb1eae3373d02fd1a25ff512f1
Tom Kedem <tompha1@hotmail.com>
2024-07-09 07:51:25 +0300

    Using the output() Function

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, computed, EventEmitter, Input, input , Output} from '@angular/core';
+import { Component, computed, input , output} from '@angular/core';
 
 
 
@@ -10,21 +10,18 @@ import { Component, computed, EventEmitter, Input, input , Output} from '@angula
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  @Input({required: true}) id!: string;
   
-  @Input({required: true}) avatar!: string;
-  @Input({required: true}) name!: string;
-  @Output() select = new EventEmitter();
-  // avatar = input.required<string>();
-  // name = input.required<string>();
+  
+  
+  select = output<string>();
+  id = input.required<string>();
+  avatar = input.required<string>();
+  name = input.required<string>();
 
-  // imagePhat = computed(() => `assets/users/${this.avatar()}`);
+  imagePhat = computed(() => `assets/users/${this.avatar()}`);
 
-  get imagePhat() {
-    return `assets/users/${this.avatar}`;
-  }
-
+ 
   onSelectedUser() {
-    this.select.emit(this.id);
+    this.select.emit(this.id());
   }
 }
